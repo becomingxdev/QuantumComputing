e3bd7afaad6ea7e3f87b72af97fd32cea69e2cc9
becomingxdev <desai.dev742@gmail.com>
2025-08-24 20:18:56 +0530

    first person genrated there qubits

--- bb84.py
+++ bb84.py
@@ -28,5 +28,36 @@ def simulate_bb84():
 
     # The rest of our code will go here...
 
-# This line runs the function when you execute the script
+    # --- Step 2: Alice encodes her bits into qubits ---
+    all_qubits = []
+    for i in range(KEY_LENGTH):
+        # Create a new circuit for each qubit
+        qc = QuantumCircuit(1, 1)
+
+
+        # Get the specific bit and basis for this round (e.g., the 5th bit and 5th basis)
+        current_bit = alice_bits[i]
+        current_basis = alice_bases[i]
+
+        # --- Z-basis logic (Basis is 0) ---
+        # A qubit always starts as 0. If Alice's bit is 1, we need to flip it.
+        if current_basis == 0 and current_bit == 1:
+            qc.x(0) # Apply X-gate to flip 0 to 1
+
+            # --- X-basis logic (Basis is 1) ---
+            # To prepare in the X-basis, we always start with a Hadamard gate.
+            # But if the bit is 1, we need to flip it first.
+        elif current_basis == 1:
+            if current_bit == 1:
+                qc.x(0) # First, flip 0 to 1
+            qc.h(0) # THEN, apply H-gate to put it in the X-basis
+            
+        all_qubits.append(qc)
+
+        #Checking for error
+        for k in all_qubits:
+            if(k == -1):
+                print("Error! restarting the process")
+
+    print(f"\nAlice has prepared {len(all_qubits)} qubits to send to Bob.")
 simulate_bb84()
