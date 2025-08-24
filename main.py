ea6206bb66f562601ace6c98d27b3ec73e8ab8de
becomingxdev <desai.dev742@gmail.com>
2025-08-24 20:40:07 +0530

    replace dummy key with real one

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-# 1. Import the FastAPI class
 from fastapi import FastAPI
+from bb84 import simulate_bb84
 
-# 2. Create an "instance" of the FastAPI application
+# Create the FastAPI app
 app = FastAPI()
 
-# 3. Define an endpoint using a "decorator"
 @app.get("/")
 def read_root():
-    # 4. Return a Python dictionary
-    return {"message": "Hello, Quantum World!"}
+    return {"message": "Welcome to the Quantum Key Distribution (QKD) Service"}
 
 @app.get("/api/v1/key")
 def get_quantum_key():
-    return {
-        "key_id": "c4a7f3e1",
-        "key_length": 16,
-        "key": "1011010011101101"
-    }
+    print("Request received: Running BB84 protocol simulation...")
+    
+    # Call the simulation function to get a key
+    secure_key = simulate_bb84()
+    
+    if secure_key is not None:
+        # If a key was generated, return it as a string
+        return {
+            "status": "success",
+            "key_length": len(secure_key),
+            "key": "".join(map(str, secure_key)) # Converts [1,0,1] to "101"
+        }
+    else:
+        # If the key is None, it means a spy was detected
+        return {
+            "status": "error",
+            "message": "Eavesdropper detected! Key generation aborted."
+        }
