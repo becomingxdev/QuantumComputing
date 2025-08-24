864e36e6e3d5ffa7a02748d44e42253798d135ab
becomingxdev <desai.dev742@gmail.com>
2025-08-24 13:09:35 +0530

    completed dictionaries

--- pythonFundamentals1.py
+++ pythonFundamentals1.py
@@ -1,6 +1,14 @@
 #API responses would be in form of json files. We are learning two important data structures. Lists and Dictionaries
 
 #Starting with lists
-
 bobBases = ['x', 'z', 'x', 'x', 'x', 'z', 'z', 'z']
 bobResult = [0, 1, 1, 1, 0, 0, 0, 1]
+
+
+
+#Dictionaries
+keyExchangeStatus = {
+    "key" : "1011001",
+    "eavesDropperDetected" : False,
+    "errorRate" : 0.02
+}
