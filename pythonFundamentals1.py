7a8d278d7ff942a37ef0fc1a9d5535815345fa07
becomingxdev <desai.dev742@gmail.com>
2025-08-24 13:32:40 +0530

    data structures done

--- pythonFundamentals1.py
+++ pythonFundamentals1.py
@@ -3,6 +3,9 @@
 #Starting with lists
 bobBases = ['x', 'z', 'x', 'x', 'x', 'z', 'z', 'z']
 bobResult = [0, 1, 1, 1, 0, 0, 0, 1]
+bit_string = "10110"
+bitList = [int(i) for i in bit_string]
+
 
 
 
@@ -11,4 +14,21 @@ keyExchangeStatus = {
     "key" : "1011001",
     "eavesDropperDetected" : False,
     "errorRate" : 0.02
+}
+
+countOf0 = 0
+countOf1 = 0
+countOfError = 0
+for i in bitList:
+    if(i == 0):
+        countOf0 += 1
+    elif(i == 1):
+        countOf1 += 1
+    else:
+        countOfError += 1
+
+results = {
+    "0" : countOf0,
+    "1" : countOf1,
+    "error" : countOfError
 }
