a0c6aa147d45448cd9b257fd543f1bfacdc697fb
becomingxdev <desai.dev742@gmail.com>
2025-08-24 13:02:15 +0530

    started fundamentals

---
+++ pythonFundamentals1.py
@@ -0,0 +1,6 @@
+#API responses would be in form of json files. We are learning two important data structures. Lists and Dictionaries
+
+#Starting with lists
+
+bobBases = ['x', 'z', 'x', 'x', 'x', 'z', 'z', 'z']
+bobResult = [0, 1, 1, 1, 0, 0, 0, 1]
